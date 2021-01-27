0cd6937b342e26c216342b2d462e0cc2cc885290
David Castillo <seth.castillo4@gmail.com>
2021-01-27 15:04:54 -0600

    Finished project

---
+++ Resume/resume.js
@@ -0,0 +1,62 @@
+function animations() {
+    var elem = document.getElementById("animate");
+  
+    elem.classList.add('animate__pulse'); 
+
+    setTimeout(function() {
+        elem.classList.remove('animate__pulse');
+      }, 5000); // 5000ms
+}
+
+function popout() {
+    var elem = document.getElementById("Picture");
+  
+    elem.classList.add('animate__zoomIn'); 
+
+    setTimeout(function() {
+        elem.classList.remove('animate__zoomIn');
+      }, 5000); // 5000ms
+}
+
+function info1() {
+    var elem = document.getElementById("icons1");
+  
+    elem.classList.add('animate__swing'); 
+
+    setTimeout(function() {
+        elem.classList.remove('animate__swing');
+      }, 5000); // 5000ms
+}
+
+
+function info2() {
+    var elem = document.getElementById("icons2");
+  
+    elem.classList.add('animate__swing'); 
+
+    setTimeout(function() {
+        elem.classList.remove('animate__swing');
+      }, 5000); // 5000ms
+}
+
+
+function info3() {
+    var elem = document.getElementById("icons3");
+  
+    elem.classList.add('animate__swing'); 
+
+    setTimeout(function() {
+        elem.classList.remove('animate__swing');
+      }, 5000); // 5000ms
+}
+
+
+function info4() {
+    var elem = document.getElementById("icons4");
+  
+    elem.classList.add('animate__swing'); 
+
+    setTimeout(function() {
+        elem.classList.remove('animate__swing');
+      }, 5000); // 5000ms
+}
